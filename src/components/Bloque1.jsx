64cc13c26ae5478baa865117f46aab7223b81677
Leglocker01 <beristaindelamora@gmail.com>
2024-05-08 16:33:36 -0600

    arreglando el ancho del boton

--- src/components/Bloque1.jsx
+++ src/components/Bloque1.jsx
@@ -4,28 +4,28 @@ import { SphereModel } from './models/SphereModel';
 
 export const Bloque1 = () => {
   return (
-    <div className=" py-10 lg:py-28 flex flex-col lg:flex-row-reverse justify-center items-center gap-5 text-center text-lg">
+    <div className="py-10 lg:py-28 flex flex-col lg:flex-row-reverse justify-center items-center gap-5 text-center text-lg">
+      <div>
         <Scene>
           <SphereModel />
         </Scene>
-      <div>
-        <div className="lg:flex flex-col gap-2 mb-5">
-          <p>
-            ¿Estás cansado de esas agencias que te hacen perder el tiempo
-            revisando publicaciones?
-          </p>
-          <p>
-            ¿Tu agencia solo te da pretextos de por qué no está generando más
-            prospectos o ventas para tu negocio?
-          </p>
-          <p>Llegaste al lugar indicado.</p>
-          <p>
-            Da clic aquí y agenda un diagnóstico de marketing totalmente{' '}
-            <span className="font-bold text-secondary">GRATIS</span>
-          </p>
-        </div>
       </div>
+      <div className="lg:flex flex-col gap-2 mb-5 text-center content-center items-center">
+        <p>
+          ¿Estás cansado de esas agencias que te hacen perder el tiempo
+          revisando publicaciones?
+        </p>
+        <p>
+          ¿Tu agencia solo te da pretextos de por qué no está generando más
+          prospectos o ventas para tu negocio?
+        </p>
+        <p>Llegaste al lugar indicado.</p>
+        <p className='mb-3'>
+          Da clic aquí y agenda un diagnóstico de marketing totalmente{' '}
+          <span className="font-bold text-secondary">GRATIS</span>
+        </p>
         <Boton btn="Agendar Diagnóstico" />
+      </div>
     </div>
   );
 };
